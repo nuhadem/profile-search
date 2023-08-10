b4e27565360c87407670f22ce4901aa44ac56720
Adem Karadag <nuhadem@gmail.com>
2023-08-10 12:50:03 +0300

    function define

---
+++ udemy_section2.js
@@ -0,0 +1,9 @@
+const name='Adem'
+let age=37
+const inQatar=false
+
+const summ=(username,age,inqatar)=>{
+return ('Name is ' + username + ' age is '+age+' in Qatar '+inQatar);
+}
+
+console.log(summ(name,age,inQatar))
