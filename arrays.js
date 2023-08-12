f7138b19689d8e49ea18f84087d185c5c94f67a4
Adem Karadag <nuhadem@gmail.com>
2023-08-13 00:43:07 +0300

    destructure

--- arrays.js
+++ arrays.js
@@ -1,4 +1,15 @@
 const hobbies=['Plants' ,'Sports', 'Cinema'];
-for (let hobby in hobbies){
-    console.log(hobby);
-}
+hobbies.push('Painting')
+//for (let hobby of hobbies){
+//    console.log(hobby);
+//}
+
+const [h1, h2]=hobbies;
+console.log(h1,h2);
+
+console.log(hobbies.map(hobby=>'Hobby: '+hobby))
+const copiedArray=hobbies.slice()
+console.log(copiedArray)
+const copiedArray2=[...hobbies]
+copiedArray2.push('Riding bicycle ')
+console.log(copiedArray2)
