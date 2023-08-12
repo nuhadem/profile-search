b42232e3b8cba1a0fca1a4077ee26e9f94e38a58
Adem Karadag <nuhadem@gmail.com>
2023-08-12 15:47:31 +0300

    this method

--- udemy_section2.js
+++ udemy_section2.js
@@ -6,4 +6,18 @@ const summ=(username,age,inqatar)=>{
 return ('Name is ' + username + ' age is '+age+' in Qatar '+inQatar);
 }
 
+const person={
+    ad: 'Adem',
+    soyad:'Karadag',
+    greeting(){
+        console.log('Hi I am '+this.ad+' '+this.soyad)
+    }
+}
+person.greeting;
+const add=(a,b)=>a+b
+const subt=a=>a-3
+const sum=()=>2*4
+console.log(add(1,4))
+console.log(subt(1))
+console.log(sum())
 console.log(summ(name,age,inQatar))
